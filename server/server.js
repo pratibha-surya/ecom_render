1b9eafbe42b38d82ac083ba172d5c2e06425d12b
pratibha-surya <suryavanshipratibha18@gmail.com>
2025-04-10 18:12:36 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -22,20 +22,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors())
 
 app.use(cookieParser());
 app.use(express.json());
