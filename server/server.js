608c1ba409dd573bb45e3a8a0a7a645bbdc57e22
pratibha-surya <suryavanshipratibha18@gmail.com>
2025-04-10 18:19:57 +0530

    cors with url

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use("/api/common/feature", commonFeatureRouter);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
